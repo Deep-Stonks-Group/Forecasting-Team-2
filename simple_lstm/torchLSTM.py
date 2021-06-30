90b64aff385fb9891a3928eccd0f5190198a3bc8
Caleb Heinzman <caleb.heinzman@gmail.com>
2021-06-30 14:36:10 -0500

    Updated library to be imported from external repo. Updated torchLSTM.py

--- simple_lstm/torchLSTM.py
+++ simple_lstm/torchLSTM.py
@@ -1,12 +1,14 @@
-import numpy as np
+import pickle
+from copy import deepcopy
+
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
+from PythonDataProcessing import DataRetrieval as DR
+from PythonDataProcessing import Metrics as MET
+from pandas.core.frame import DataFrame
 from sklearn.preprocessing import MinMaxScaler
-import talib as ta
-import sklearn.metrics as skm
-from lib import DataRetrieval as DR
-from lib import Metrics as MET
 
 
 class LSTM(nn.Module):
@@ -25,6 +27,8 @@ class LSTM(nn.Module):
 
         self.fc = nn.Linear(hidden_size, num_classes)
 
+        self.trained_tickers = []
+
     def forward(self, x):
         h_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
         c_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
@@ -33,7 +37,7 @@ class LSTM(nn.Module):
         out = self.fc(h_out)
         return out
 
-def lstm_data_prep(data, labels, seq_length):
+def sliding_windows( data, labels, seq_length):
     x = []
     y = []
     for i in range(data.shape[0]-seq_length-1):
@@ -43,72 +47,154 @@ def lstm_data_prep(data, labels, seq_length):
         y.append(_y)
     return x, y
 
-# I want to eventually standardize the rest of this data preparation so that it can be used by other teams as well.
-data_scaler = MinMaxScaler()
-labl_scaler = MinMaxScaler()
+def retrieve_stock_data(ticker: str, input_dims, label_dims) -> DataFrame:
+    # Loading Data
+    # data_source = DR.get_crypto_data('ETH-BTC',interval='1h',start_date='2021-04-01-00-00')
+    data_source = DR.get_stock_data(ticker,interval='1d',period='2y')
+    stock_dataframe = deepcopy(data_source)
+    new_features = []
+    new_features.extend(set(input_dims) - set(data_source.columns))
+    new_features.extend(set(label_dims) - set(data_source.columns))
 
-# Loading Data
-data_source = DR.get_crypto_data('ETH-BTC',interval='1h',start_date='2021-04-01-00-00')
-# data_source = DR.get_stock_data('DIA',interval='1d',period='2y')
+    for new_feature in new_features:
+        try:
+            stock_dataframe = DR.add_technical_indicators[new_feature](data_source)
+        except KeyError:
+            print(f'No function exists to add the dimension {new_feature}')
+            raise
 
-# Adding EMA/SMA to the data
-data_source = DR.add_SMA(data_source)
-data_source = DR.add_EMA(data_source)
-
-# Date High Low Open Close Volume AdjClose
-scaled_data = data_scaler.fit_transform(data_source[['High','Low','Close','Volume','EMA']])
-scaled_lbls = labl_scaler.fit_transform(data_source[['SMA']])
-#normed = data_source[data_source.columns[1:]].apply(lambda x: x/x.max())
-
-
-seq_length = 10
-x, y = lstm_data_prep(scaled_data, scaled_lbls, seq_length)
-dataX = torch.Tensor(np.array(x))
-dataY = torch.Tensor(np.array(y))
-train_size = int(len(y) * 0.75)
-train_x = torch.Tensor(x[0:train_size])
-train_y = torch.Tensor(y[0:train_size])
-test_x = torch.Tensor(x[train_size:])
-test_y = torch.Tensor(y[train_size:])
-
-input_size = 5
-hidden_size = 6
-num_layers = 1
-num_classes = 1
-epochs = 2000
-learning_rate = 0.01
-
-lstm = LSTM(num_classes, input_size, hidden_size, num_layers, seq_length)
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(lstm.parameters(), lr=learning_rate)
-
-"""TRAIN"""
-lstm.train()
-for epoch in range(epochs):
-    outputs = lstm(train_x)
-    optimizer.zero_grad()
-    loss = criterion(outputs, train_y)
-    loss.backward()
-    optimizer.step()
-    if epoch % 100 == 0:
-      print("Epoch: %d, loss: %1.5f" % (epoch, loss.item()))
-
-"""TEST"""
-lstm.eval()
-train_predict = lstm(dataX)
-data_predict = train_predict.data.numpy()
-dataY_plot = dataY.data.numpy()
-
-data_predict = labl_scaler.inverse_transform(data_predict)
-dataY_plot = labl_scaler.inverse_transform(dataY_plot.reshape(dataY_plot.shape[0],1))
-
-plt.axvline(x=train_size, c='r', linestyle='--')
-plt.plot(dataY_plot)
-plt.plot(data_predict)
-plt.suptitle('Time-Series Prediction')
-plt.show()
+    return stock_dataframe
 
 
-print('done')
+def fit_scalers(data_source: DataFrame, train_size, input_dims, label_dims):
+    data_scaler = MinMaxScaler()
+    labl_scaler = MinMaxScaler()
 
-MET.print_metrics(test_x,test_y,lstm,labl_scaler)
+    data_scaler.fit(data_source[input_dims].iloc[:train_size])
+    labl_scaler.fit(data_source[label_dims].iloc[:train_size])
+
+    return data_scaler, labl_scaler
+
+
+def normalize_stock_data(data_source, data_scaler, labl_scaler, input_dims, label_dims):
+    scaled_data = data_scaler.transform(data_source[input_dims])
+    scaled_lbls = labl_scaler.transform(data_source[label_dims])
+    return scaled_data, scaled_lbls
+
+
+
+def train_model(lstm, train_x, train_y, epochs=2000, learning_rate=.01):
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(lstm.parameters(), lr=learning_rate)
+    lstm.train()
+    for epoch in range(epochs):
+        outputs = lstm(train_x)
+        optimizer.zero_grad()
+        loss = criterion(outputs, train_y)
+        loss.backward()
+        optimizer.step()
+        if epoch % 100 == 0:
+            print("Epoch: %d, loss: %1.5f" % (epoch, loss.item()))
+
+class PredictionEngine():
+
+    def __init__(self, input_dims=['High','Low','Close','Volume','EMA'], label_dims=['SMA'], seq_length=10):
+        self.input_dims = input_dims
+        self.label_dims = label_dims
+        self.seq_length = seq_length
+
+    def create_model(self, input_size=5, hidden_size=6, num_layers=1, num_classes=1):
+        self.lstm = LSTM(num_classes, input_size, hidden_size, num_layers, self.seq_length)
+
+    def save_model(self):
+        self.lstm.trained_tickers.sort()
+        name = ''.join(self.lstm.trained_tickers)
+        with open('simple_lstm/models/' + name + '.p', 'wb') as outfile:
+            pickle.dump(self.lstm, outfile)
+
+    def load_model(self, name):
+        if name:
+            try:
+                with open('simple_lstm/models/' + name + '.p', 'rb') as infile:
+                    self.lstm = pickle.load(infile)
+            except Exception as e:
+                print('could not load model {}'.format(name))
+                raise
+
+    def train_ticker(self, ticker: str, training_set_coeff=0.8):
+        if not self.lstm:
+            self.create_model()
+        self.lstm.train()
+        if ticker in self.lstm.trained_tickers:
+            print(f'Already trained model on {ticker}')
+            return
+        stock_dataframe = retrieve_stock_data(ticker, self.input_dims, self.label_dims)
+        train_size = int(len(stock_dataframe) * training_set_coeff)
+        data_scaler, label_scaler = fit_scalers(stock_dataframe, train_size, self.input_dims, self.label_dims)
+        scaled_data, scaled_labels = normalize_stock_data(stock_dataframe, data_scaler, label_scaler, self.input_dims, self.label_dims)
+        x, y = sliding_windows(scaled_data, scaled_labels, self.seq_length)
+        train_x = torch.Tensor(x[0:train_size])
+        train_y = torch.Tensor(y[0:train_size])
+        train_model(self.lstm, train_x, train_y)
+        self.lstm.trained_tickers.append(ticker)
+
+    def eval_ticker(self, ticker, training_set_coeff=0.8):
+        self.lstm.eval()
+
+        stock_dataframe = retrieve_stock_data(ticker, self.input_dims, self.label_dims)
+        train_size = int(len(stock_dataframe) * training_set_coeff)
+        data_scaler, label_scaler = fit_scalers(stock_dataframe, train_size, self.input_dims, self.label_dims)
+        scaled_data, scaled_labels = normalize_stock_data(stock_dataframe, data_scaler, label_scaler, self.input_dims, self.label_dims)
+        
+        x, y = sliding_windows(scaled_data, scaled_labels, self.seq_length)
+        dataX = torch.Tensor(np.array(x))
+        dataY = torch.Tensor(np.array(y))
+        test_x = torch.Tensor(x[train_size:])
+        test_y = torch.Tensor(y[train_size:])
+
+        all_predict = self.lstm(dataX)
+        data_predict = all_predict.data.numpy()
+        dataY_plot = dataY.data.numpy()
+
+        data_predict = label_scaler.inverse_transform(data_predict)
+        dataY_plot = label_scaler.inverse_transform(dataY_plot.reshape(dataY_plot.shape[0],1))
+
+        plt.axvline(x=train_size, c='r', linestyle='--')
+        plt.plot(dataY_plot)
+        plt.plot(data_predict)
+        plt.suptitle('Time-Series Prediction')
+        plt.show()
+        MET.print_metrics(test_x,test_y,self.lstm,label_scaler)
+
+    def predict(self, ticker, input_sequence):
+        # would like to not fit scalers every time but I dont want to store them anywhere yet
+        stock_dataframe = retrieve_stock_data(ticker, self.input_dims, self.label_dims)
+        train_size = len(stock_dataframe)
+        data_scaler, label_scaler = fit_scalers(stock_dataframe, train_size, self.input_dims, self.label_dims)
+        scaled_input_sequence = data_scaler.transform(input_sequence)
+        output = self.lstm(scaled_input_sequence)
+        prediction = label_scaler.inverse_transform(output.data.numpy)
+        return prediction
+
+
+""" USAGE EXAMPLE """
+
+predictor = PredictionEngine()
+predictor.create_model()
+
+""" training on multiple tickers produces far less accurate eval results than one ticker per model
+
+training_set_coeff = 0.8
+top_stocks = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'FB', 'CMCSA', 'JPM', 'HD', 'DIS', 'XOM']
+for stock in top_stocks:
+    model_1.train_ticker(stock, training_set_coeff)
+
+"""
+
+predictor.train_ticker('GOOGL')
+predictor.train_ticker('FB')
+
+predictor.eval_ticker('FB')
+predictor.save_model()
+
+
